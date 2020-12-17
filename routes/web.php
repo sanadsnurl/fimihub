bcd3975fce22e8a238a58b08ad90f578e5472087
spidy56 <72739191+spidy56@users.noreply.github.com>
2020-12-17 15:42:51 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
 
     // Customer Root Page
     Route::get('/', function () {
+        return public_path();
         return view('customer.index');
     });
     // Customer Login
