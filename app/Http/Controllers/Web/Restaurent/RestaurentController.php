b6e6a2a2cf9063f915fff800efe71979122af80e
spidy56 <72739191+spidy56@users.noreply.github.com>
2020-12-18 11:14:15 +0530

    Update RestaurentController.php

--- app/Http/Controllers/Web/Restaurent/RestaurentController.php
+++ app/Http/Controllers/Web/Restaurent/RestaurentController.php
@@ -31,7 +31,7 @@ class RestaurentController extends Controller
         $resto_data = $restaurent_detail->getRestoData($user->id);
         $user_address = new user_address;
         $resto_add = $user_address->getUserAddress($user->id);
-        echo($user->id);
+        echo($resto_data);
         die();
         if($resto_add == NULL){
             return view('restaurent.myDetails')->with(['data'=>$user,
